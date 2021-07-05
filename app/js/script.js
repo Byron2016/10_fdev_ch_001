aa75ffa67eb18bde49c3062b1f3eba172bd1fc8c
Byron 2016 <bgva2005@yahoo.com>
2021-07-05 11:38:44 -0500

    P02_Overlay desplegar menu y reposicionamiento de Animacion ícono hamburger a X (58.38)

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,12 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+
 btnHamburger.addEventListener('click', function(){
   console.log("aa")
-  if(btnHamburger.classList.contains('open')){
-    btnHamburger.classList.remove('open')
+  if(header.classList.contains('open')){
+    header.classList.remove('open')
   }
   else {
-    btnHamburger.classList.add('open')
+    header.classList.add('open')
   }
 })
