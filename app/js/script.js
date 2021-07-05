2cbc45985426f6c8c5e67ee56aa420522e28353a
Byron 2016 <bgva2005@yahoo.com>
2021-07-05 12:38:37 -0500

    P03_Cambiar nombre clase __menu a __toggle (1.21.29)

--- app/js/script.js
+++ app/js/script.js
@@ -4,12 +4,12 @@ const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener('click', function(){
   console.log("aa");
-  if(header.classList.contains('open')){ // close hamburguer menu
+  if(header.classList.contains('open')){ // close hamburguer toggle
     header.classList.remove('open');
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
   }
-  else { // open hamburguer menu
+  else { // open hamburguer toggle
     header.classList.add('open');
     overlay.classList.remove('fade-out');
     overlay.classList.add('fade-in');
