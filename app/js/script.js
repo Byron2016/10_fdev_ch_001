1f356a3064bdfab039c8a579677b338998f4cf24
Byron 2016 <bgva2005@yahoo.com>
2021-07-05 12:23:25 -0500

    P02_Animación de donde se despliega menu, cambio de color(1.05.43)

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,17 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 btnHamburger.addEventListener('click', function(){
-  console.log("aa")
-  if(header.classList.contains('open')){
-    header.classList.remove('open')
+  console.log("aa");
+  if(header.classList.contains('open')){ // close hamburguer menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
   }
-  else {
-    header.classList.add('open')
+  else { // open hamburguer menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
   }
 })
